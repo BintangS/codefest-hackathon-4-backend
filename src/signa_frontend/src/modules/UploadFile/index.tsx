cce684044097b83f68d38302a7b0a0ab95d251b9
ichwan haryo sembodo <ichwanharyosembodo96@gmail.com>
2024-06-24 11:30:01 +0900

    add documentation on how to develop signa locally

--- src/signa_frontend/src/modules/UploadFile/index.tsx
+++ src/signa_frontend/src/modules/UploadFile/index.tsx
@@ -3,12 +3,6 @@ import DragAndDropUpload from '../../components/dragAndDropUpload';
 const UploadFileModule = () => {
   return (
     <>
-      <h1>
-        Signa!
-      </h1>
-      <p>
-        Trusted, Decentralized, and Easy to use document signing on the Blockchain. <br />
-      </p>
       <DragAndDropUpload />
     </>
   );
