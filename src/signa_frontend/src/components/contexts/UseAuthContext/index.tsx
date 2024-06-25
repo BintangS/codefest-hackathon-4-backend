ed2c50ae3d067ce4fd65ea77db242d5e7319b3e6
BintangS <bintangsubuh@hotmail.com>
2024-06-25 21:25:47 +0700

    Finish Implement Login Page Design

--- src/signa_frontend/src/components/contexts/UseAuthContext/index.tsx
+++ src/signa_frontend/src/components/contexts/UseAuthContext/index.tsx
@@ -1,8 +1,7 @@
 import React, { createContext, useContext, useState } from 'react';
 import {
   AuthContextInterface,
-  AuthContextProviderProps,
-  userProfileDataInterface
+  AuthContextProviderProps
 } from './interface';
 // import { signa_backend } from '@backend';
 // import { AuthClient } from '@dfinity/auth-client';
@@ -14,30 +13,27 @@ export const useAuthContext = () => useContext(AuthContext);
 
 export const AuthContextProvider: React.FC<AuthContextProviderProps> = ({children}) => {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>();
-  const [userProfileData, setUserProfileData] = useState<userProfileDataInterface | null>(null);
+  // const [userProfileData, setUserProfileData] = useState<userProfileDataInterface | null>(null);
 
-  const login = (email: string, password: string) => {
+  const login = () => {
     if (!isAuthenticated) {
-      if (email == "steve.harnadi@gmail.com" && password == "admin123") {
-        setUserProfileData({
-          email : email,
-          password : password
-        })
-        localStorage.setItem("identityICP", "abcdefghi")
-        setIsAuthenticated(true);
-      }
+      // setUserProfileData({
+      //   email : email,
+      //   password : password
+      // })
+      localStorage.setItem("identityICP", "abcdefghi")
+      setIsAuthenticated(true);
     }
   };
 
   const logout = () => {
-    setUserProfileData(null)
+    // setUserProfileData(null)
     localStorage.removeItem("identityICP")
     setIsAuthenticated(false);
   };
 
   const contextValue = {
     isAuthenticated: isAuthenticated,
-    userProfileData: userProfileData,
     login: login,
     logout: logout
   };
